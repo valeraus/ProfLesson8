2394656b3a5070d65da66de3a713401d7fbc5e0e
Valera <vlaus0@gmail.com>
2023-04-14 23:11:00 +0300

    Добавьте файлы проекта.

---
+++ AddTask/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTask
+{
+    /*
+     Створіть користувацький тип (наприклад, клас) і виконайте серіалізацію об’єкта цього типу,
+    враховуючи той факт, що стан об’єкту необхідно передати по мережі.
+     */
+    [Serializable]
+    class Car
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+
+        public Car(string name, int speed)
+        {
+            Name = name;
+            Speed = speed;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Car auto = new Car("Mercedes", 250);
+
+            FileStream stream = File.Create("CarData.data");
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            // Cериализация.
+            formatter.Serialize(stream, auto);
+            stream.Close();
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Task3
+{
+    /*
+     Створіть новий додаток, в котрому виконайте серіалізацію об’єкту з попереднього прикладу.
+    Відобразіть стан об’єкту на екрані.
+     */
+    public class Car
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+
+        public Car(string name, int speed)
+        {
+            Name = name;
+            Speed = speed;
+        }
+
+        public Car()
+        {
+
+        }
+    }
+
+    public class CarXML
+    {
+        [XmlAttribute]
+        public string Name { get; set; }
+        [XmlAttribute]
+        public int Speed { get; set; }
+
+        public CarXML(string name, int speed)
+        {
+            Name = name;
+            Speed = speed;
+        }
+
+        public CarXML()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Car));
+
+            FileStream original = new FileStream("SerializationOriginal.xml", FileMode.Open, FileAccess.Read);
+
+            Car auto = (Car)serializer.Deserialize(original);
+
+            original.Close();
+
+            Console.WriteLine(auto.Name + " " + auto.Speed);
+
+            XmlSerializer serializerXML = new XmlSerializer(typeof(CarXML));
+
+            FileStream streamXML = new FileStream("SerializationXML.xml", FileMode.Open, FileAccess.Read);
+
+            CarXML autoXML = (CarXML)serializerXML.Deserialize(streamXML);
+
+            streamXML.Close();
+
+            Console.WriteLine(autoXML.Name + " " + autoXML.Speed);
+        }
+    }
+}
+
